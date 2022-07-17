6dfc2b9141609a1774658a5416e87798d3edf50c
davidjumiller <davidjumiller@gmail.com>
2022-07-17 16:49:54 -0700

    Added morgan middleware for logging http requests (3.7-3.8)

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('body', function getBody(req) {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
